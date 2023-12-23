8befdb28f4e303991a587dfa78993b29b5af2c7d
Lukas Deward <lukas@deward.de>
2023-12-23 17:14:56 +0100

    rerun deployment

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <p>Hallo welt</p>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
@@ -23,9 +24,11 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
